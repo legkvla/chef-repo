55a2ac1e2efc5c50c685489fa26ae49d72484b2a
Vladimir Legkunets <vladimir.legkunets@gmail.com>
2015-04-30 13:26:37 +0000

    Submitting company details for certificate

--- config/rake.rb
+++ config/rake.rb
@@ -6,7 +6,7 @@
 ###
 
 # The company name - used for SSL certificates, and in srvious other places
-COMPANY_NAME = "Example Com"
+COMPANY_NAME = "RiskGap.com"
 
 # The Country Name to use for SSL Certificates
 SSL_COUNTRY_NAME = "US"
@@ -15,13 +15,13 @@ SSL_COUNTRY_NAME = "US"
 SSL_STATE_NAME = "Several"
 
 # The Locality Name for SSL - typically, the city
-SSL_LOCALITY_NAME = "Locality"
+SSL_LOCALITY_NAME = "San Francisco"
 
 # What department?
-SSL_ORGANIZATIONAL_UNIT_NAME = "Operations"
+SSL_ORGANIZATIONAL_UNIT_NAME = "Production"
 
 # The SSL contact email address
-SSL_EMAIL_ADDRESS = "ops@example.com"
+SSL_EMAIL_ADDRESS = "info@riskgap.com"
 
 # License for new Cookbooks
 # Can be :apachev2 or :none
